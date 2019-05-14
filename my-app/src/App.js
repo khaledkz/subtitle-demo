5b7e7f06363b22a48e67d2762d2ad01278e19d0e
Khaled <kha.1991.14.2017@gmail.com>
2019-05-14 14:14:26 +0100

    init express server

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import Player from './components/player.js'
 
 function App() {
--- my-app/src/components/player.js/Player.js
+++ my-app/src/components/player.js/Player.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 
 
 class Welcome extends Component {
-  render(){
-    return 'Video'
+  render() {
+    // "http://bcboltstv-a.akamaihd.net/media/v1/pmp4/static/clear/1486976045/f44e8fe0-7900-44d9-84a6-decf7d83edf8/high.mp4"
+    // https://files.stvqa.tv/player/subtitles/emmerdale-emmerdale-ep-1352019-23413.subs.vtt
+    return (
+      <figure id="videoContainer">
+        <video id="video" controls preload="metadata" poster="img/poster.jpg">
+          <source src="http://bcboltstv-a.akamaihd.net/media/v1/pmp4/static/clear/1486976045/f44e8fe0-7900-44d9-84a6-decf7d83edf8/high.mp4" type="video/mp4" />
+          <track id="subtitleTrack" kind="subtitles" label="English subtitles" src="https://files.stvqa.tv/player/subtitles/emmerdale-emmerdale-ep-1352019-23413.subs.vtt" srclang="en" default>
+          </track>
+          <track id="subtitleTrack" kind="subtitles" label="Arabic subtitles" src="https://files.stvqa.tv/player/subtitles/emmerdale-emmerdale-ep-1352019-23413.subs.vtt" srclang="ar" default>
+          </track>
+          <track id="subtitleTrack" kind="subtitles" label="France subtitles" src="https://files.stvqa.tv/player/subtitles/emmerdale-emmerdale-ep-1352019-23413.subs.vtt" srclang="fr" default>
+          </track>
+          </video>
+      </figure>
+    )
   }
 }
 
